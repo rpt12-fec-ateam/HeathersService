91a1092426f0700a64f3196c2a3a0f9128ad3764
Heather Piverotto <heather@Huberts-MacBook-Pro.local>
2019-04-01 17:59:00 -0700

    adding testing updates for db and server

--- test/dbTesting.js
+++ test/dbTesting.js
@@ -1,5 +1,27 @@
-//require statements
+var chai = require('chai');
+var should = chai.should();
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+
+//require db file with query
 
 describe('FEC DB', function() {
   
+     //check for types of each value in db schema
+    it('should have specific data types for each property of the item', () => {
+      //reference db query
+      ((err, results) => {
+          //code to check each value of each prop
+          done();
+      })
+    });
+
+    it('should have at least 100 items stored', (done) => {
+        //reference db query
+        ((err, results) => {
+          assert.isAtLeast(results.length, 100)
+          done();
+        });
+    });
+
 });
--- test/serverTesting.js
+++ test/serverTesting.js
@@ -1,5 +1,31 @@
-//require statements
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server/index.js');
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+var should = chai.should();
 
-describe('FEC Server', function() {
-  
+chai.use(chaiHTTP);
+
+describe('FEC Server', () => {
+
+    it('should list ALL ingredients on /ingredients GET', function(done) {
+        chai.request(server)
+            .get('/ingredients')
+            .end((err, res) => {
+              res.should.have.status(200);
+              done();
+            });
+        });
+    
+    it('should respond with a list of at least 100 items', () => {
+      chai.request(server)
+        .get('/ingredients')
+        .end((err, res) => {
+          assert.isAtLeast(res.body.ingredients.length, 100)
+          done();
+        });
+    });
+    
+    
 });
