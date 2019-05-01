ad7c2738a5f275ec86135a3f097f4dd01e56aa55
Heather Piverotto <heather@Huberts-MacBook-Pro.local>
2019-04-30 19:16:24 -0700

    making key ingredients bold

--- client/keyIngredients.jsx
+++ client/keyIngredients.jsx
@@ -4,11 +4,16 @@ import styled from 'styled-components';
 const HalfPos = styled.div`
   margin-left: 45%;
 `;
+
+const Bold = styled.div`
+  font-weight:bold;
+`;
+
 const keyIngredients = (props) => {
   const filler = props.data.nutritionFacts;
   const keyArr = props.data.keyIngredients.split(' ');
   const keys = keyArr.map((key, index) => {
-    return <div><div>{key}</div><div>{filler}</div></div>
+    return <div><Bold>{key}</Bold><div>{filler}</div></div>
   })
   return (
       <HalfPos>{keys}</HalfPos>
